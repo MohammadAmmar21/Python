8ebe2d0c4dc2b286c7d20493fafe56a19b43f73e
juststartedcoding21 <71547331+juststartedcoding21@users.noreply.github.com>
2020-10-07 20:58:17 +0530

    Create matrix_transporse.py

---
+++ matrix_transporse.py
@@ -0,0 +1,21 @@
+##transporseofmatrix
+
+rows,cols=map(int,input().split(" "))
+print(rows,cols)
+mat1=[]
+mat2=[]
+for i in range(rows):
+    temp=list(map(int,input().split(" ")))
+    mat1.append(temp)
+print(mat1)
+for i in mat1:
+    print(" ".join(map(str,i)))
+for i in range(rows):
+    row=[]
+    for j in range(cols):
+        temp=mat1[j][i]
+        row.append(temp)
+    mat2.append(row)
+print(mat2)
+for i in mat2:
+    print(" ".join(map(str,i)))
