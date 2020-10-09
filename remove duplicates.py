fe6deb3db3a7217c5ef8275d65b185418cd6d6df
juststartedcoding21 <71547331+juststartedcoding21@users.noreply.github.com>
2020-10-09 09:46:14 +0530

    Update remove duplicates.py

--- remove duplicates.py
+++ remove duplicates.py
@@ -20,3 +20,11 @@ for i in lis:
         dic[i]='yes'
         Ans.append(i)
 print(Ans)
+
+lis = [1,65,2,12,4,5,3]
+n = len(lis)
+for i in range(n):
+    for j in range(n-i-1):
+        if lis[j] > lis[j+1]: 
+            lis[j], lis[j+1] = lis[j+1], lis[j]
+print(lis)
