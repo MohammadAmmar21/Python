5a5e11d1f7882787d756714895128f790b4a2d11
juststartedcoding21 <71547331+juststartedcoding21@users.noreply.github.com>
2020-10-22 20:19:06 +0530

    Create hcf.py

---
+++ hcf.py
@@ -0,0 +1,12 @@
+##hcf
+
+n1=int(input())
+n2=int(input())
+if(n1>n2):
+    smaller=n1
+else:
+    smaller=n2
+    for i in range(1,smaller+1):
+        if(n1%i==0 and n2%i==0):
+            hcf=i
+    print(hcf)
